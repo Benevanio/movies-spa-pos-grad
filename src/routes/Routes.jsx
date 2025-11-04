b12d539e219f8f9f59a2d335b458eb211e6c410d
Benevanio Santos <benevanioconceicao.sysmap@natura.net>
2025-11-04 20:25:18 -0300

    Add routing and layout components; remove unused styles and tests
    - Integrated react-router-dom for navigation
    - Created Header, RootLayout, Home, and Movies components
    - Established routes for Home and Movies pages
    - Removed App.css, index.css, App.test.js, and setupTests.js for a cleaner structure
    - Updated package.json and package-lock.json to include react-router-dom

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../views/Home";
+import Movies from "../views/Movies";
+import RootLayout from "../views/RootLayout";
+
+export const routes = createBrowserRouter([
+
+    {
+        path: "/",
+        element: <RootLayout />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+            {
+                path: "movies",
+                element: <Movies />
+            },
+        ]
+
+    }
+]
+);
